181fd3bd389519bae3ac3843d9af513cd3a69802
Rich Parrish <rparrish@brightroll.com>
2014-08-01 23:10:57 -0400

    Add combine_latest function

--- test/streamz_test.exs
+++ test/streamz_test.exs
@@ -17,4 +17,31 @@ defmodule StreamzTest do
     GenEvent.sync_notify(event_one, 5)
     assert Task.await(task) == [1,2,3]
   end
+
+  test "combine_latest/2" do
+    left = Stream.cycle([
+      %{:color => "red", :shape => "triangle"},
+      %{:color => "blue", :shape => "triangle"},
+      %{:color => "green", :shape => "diamond"}
+    ])
+
+    right = Stream.cycle([
+      %{:color => "yellow", :shape => "square"},
+      %{:color => "red", :shape => "circle"},
+      %{:color => "orange", :shape => "square"}
+    ])
+
+    combined = Streamz.combine_latest(left,right)
+
+    result = combined
+    |> Stream.map(fn({a,b}) -> %{:color => a.color, :shape => b.shape} end)
+    |> Enum.take(4)
+
+    assert result == [
+      %{color: "red", shape: "square"},
+      %{color: "blue", shape: "square"},
+      %{color: "blue", shape: "circle"},
+      %{color: "green", shape: "circle"}
+    ]
+  end
 end
